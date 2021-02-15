d5a796a5bb5b9598ff9f64c8651def865be5d297
agentraghav <raghvendrarathore2@gmail.com>
2021-02-15 14:30:04 +0530

    extract body fields

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -316,9 +316,11 @@ exports.book_update_get = function (req, res, next) {
 
 exports.book_update_post = [
   (req, res, next) => {
-    if (!(req.body.genre instanceof Array)) {
-      if (typeof req.body.genre === 'undefined') req.body.genre = [];
-      else req.body.genre = new Array(req.body.genre);
+    const { genre } = req.body;
+
+    if (!(genre instanceof Array)) {
+      if (typeof genre === 'undefined') genre = [];
+      else genre = new Array(genre);
     }
     next();
   },
